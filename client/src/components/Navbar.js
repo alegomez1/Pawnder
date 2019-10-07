3f7f27181ef814fdd44c78d079ce438cc04a8029
alejandrogomez <alegomez1@me.com>
2019-10-07 15:44:34 -0400

    Restructured code

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Navbar extends Component {
+  render () {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a href="#" className="Navbar-brand">Pawnder</a>
+        </nav>
+      </div>
+    )
+  }
+}
+
+export default Navbar
--- server/configs/database.js
+++ server/configs/database.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 
 // Don't forget to set "MONGODB_URI" in ~/server/.env
 const uri =
-  process.env.MONGODB_URI ||
-  `mongodb://localhost/please-set-process-env-mongodb-uri`
+  process.env.MONGODB_URI || 
+  `mongodb://localhost/Pawnder`
 
 mongoose
   .connect(uri, { useNewUrlParser: true })
