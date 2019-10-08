bd8e8a0ccf10e17217cd328f0553157fbf8ef4d3
alejandrogomez <alegomez1@me.com>
2019-10-08 09:43:03 -0400

    Added routes for users

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,12 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Home extends Component {
+
+  state={
+    user: 'Alex'
+  }
   render() {
     return (
       <div className="Home">
-        <h2>Home</h2>
+        <h2>Welcome to Pawnder! {this.state.user}</h2>
       </div>
     )
   }
 }
-
