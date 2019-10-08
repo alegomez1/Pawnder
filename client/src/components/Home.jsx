b6e1888db33c2ccbcf8737a36dbbadcfa928510a
alejandrogomez <alegomez1@me.com>
2019-10-08 09:53:42 -0400

    Home page welcomes user by username

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import api from '../api'
 
 export default class Home extends Component {
 
   state={
-    user: 'Alex'
+    user: ''
+  }
+  async componentDidMount(){  
+  let current = await api.getLocalStorageUser()
+  this.setState({
+    user:current.username
+  })
   }
   render() {
+
+    
     return (
       <div className="Home">
         <h2>Welcome to Pawnder! {this.state.user}</h2>
