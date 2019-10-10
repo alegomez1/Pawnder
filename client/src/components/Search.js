b356690bd2da6d4121290c0d93028ace77aa135a
alejandrogomez <alegomez1@me.com>
2019-10-10 14:16:29 -0400

    Mapping through search terms works

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -2,38 +2,57 @@ import React, { Component } from 'react'
 import Axios from 'axios'
 
 class Search extends Component {
-  state={
-    ownerName: '',
-    search: ''
+  state = {
+    numberOfResults: '',
+    search: '',
+    results: [],
   }
 
-  search = () =>{
-    Axios.get(`http://localhost:5000/api/users/${this.state.search}`)
-    .then(result=>{
-      console.log('Search term:', this.state.search)
-      console.log(result)
-      this.setState({
-        ownerName: result.data.length
-      })
-    })
+  search = () => {
+    Axios.get(`http://localhost:5000/api/users/${this.state.search}`).then(
+      results => {
+        console.log(results.data)
+        let allUsers = results.data
+        allUsers.map((eachUser)=>{
+          console.log('each user:', eachUser)
+        })
+        this.setState({
+          numberOfResults: results.data.length,
+          results: allUsers
+        })
+        console.log('CHECK RESULTS STATE', this.state.results)
+      }
+    )
   }
 
-  handleChange = (e) =>{
+  displayUsers = () => {
+    console.log('display users function')
+    console.log(this.results)
+  }
+  //  result.map((eachUser,i)=>{
+  //   return <div>{eachUser.data[i].ownerName}</div>
+  // })
+
+  handleChange = e => {
     this.setState({
-      [e.target.name] : e.target.value
+      [e.target.name]: e.target.value,
     })
   }
 
-  render () {
-
-
-
+  render() {
     return (
       <div>
-        <input type='text' placeholder="Search city" name="search" onChange={this.handleChange}></input>
-        <button type='text' onClick={this.search}>Search</button>
+        <input
+          type="text"
+          placeholder="Search city"
+          name="search"
+          onChange={this.handleChange}
+        ></input>
+        <button type="text" onClick={this.search}>
+          Search
+        </button>
         <div>
-          <h2>Owner info: {this.state.ownerName}</h2>
+          <h2>Number of results: {this.state.numberOfResults}</h2>
         </div>
       </div>
     )
