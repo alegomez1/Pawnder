9a4a2460bd00ee9189e5edb8f2934ae88618a563
alejandrogomez <alegomez1@me.com>
2019-10-10 13:04:49 -0400

    Search works properly based on input in search bar

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -4,26 +4,34 @@ import Axios from 'axios'
 class Search extends Component {
   state={
     ownerName: '',
-    search: 'Miami'
+    search: ''
   }
+
   search = () =>{
     Axios.get(`http://localhost:5000/api/users/${this.state.search}`)
     .then(result=>{
+      console.log('Search term:', this.state.search)
       console.log(result)
       this.setState({
-        ownerName: result.data[0].ownerName
+        ownerName: result.data.length
       })
     })
   }
+
+  handleChange = (e) =>{
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+
   render () {
 
 
 
     return (
       <div>
-        <input type='text' placeholder="Search city"></input>
+        <input type='text' placeholder="Search city" name="search" onChange={this.handleChange}></input>
         <button type='text' onClick={this.search}>Search</button>
-
         <div>
           <h2>Owner info: {this.state.ownerName}</h2>
         </div>
