ffdf601d0531291851998ca0e8553a943dcb366c
alejandrogomez <alegomez1@me.com>
2019-10-10 12:03:38 -0400

    Server side search works with route

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 class Search extends Component {
+  state={
+    ownerName: '',
+    search: 'Miami'
+  }
+
+  // axios.get('')
+
   render () {
+
     return (
       <div>
         <input type='text' placeholder="Search city"></input>
+
+        <div>
+          <h2>Owner info: </h2>
+        </div>
       </div>
     )
   }
