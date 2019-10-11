66bfda03a5da88aac0ead951355c8037dad49bd7
alejandrogomez <alegomez1@me.com>
2019-10-11 09:38:35 -0400

    Unique page shows owner info now

--- client/src/components/UniqueUser.js
+++ client/src/components/UniqueUser.js
@@ -2,33 +2,31 @@ import React, { Component } from 'react'
 import Axios from 'axios'
 
 class UniqueUser extends Component {
-  state={
-    user: ''
+  state = {
+    ownerName: '',
   }
 
   componentDidMount() {
-
     let id = this.props.match.params.id
+
+    console.log('IN UNIQUE USER')
 
     Axios.get(`http://localhost:5000/api/users/${id}`)
       .then(response => {
         console.log(response)
         this.setState({
-          user: response.data[0].ownerName
+          ownerName: response.data.user[0].ownerName,
         })
       })
       .catch(err => console.log(err))
   }
   render() {
-    return <div>
-      <h1>Profile of {this.state.user}</h1>
-    </div>
+    return (
+      <div>
+        <h1>Profile of {this.state.ownerName}</h1>
+      </div>
+    )
   }
 }
 
 export default UniqueUser
-
-
-
-
-
