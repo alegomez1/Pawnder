43d925a4c8a206590783b1b783dba020d50a105c
alejandrogomez <alegomez1@me.com>
2019-10-11 11:10:30 -0400

    heroku stuff

--- server/configs/database.js
+++ server/configs/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 // Don't forget to set "MONGODB_URI" in ~/server/.env
 const uri =
-  `mongodb+srv://alex:alex@cluster0-ci3y7.mongodb.net/test?retryWrites=true&w=majority` ||
+  `mongodb+srv://alex:alex@cluster0-ci3y7.mongodb.net/pawnder?retryWrites=true&w=majority` ||
   `mongodb://localhost/Pawnder`
 
 console.log(uri, '=-=-=-==-=-')
