d425b6b91fc5ab18713f13daffdaf4ea6193cb55
alejandrogomez <alegomez1@me.com>
2019-10-12 14:58:00 -0400

    Search properly maps through all users

--- client/src/components/Search.js
+++ client/src/components/Search.js
@@ -10,31 +10,56 @@ class Search extends Component {
     numberOfResults: '',
     search: '',
     results: [],
+    actualResults: []
   }
 
-  search = () => {
+  search = async () => {
     // Axios.get(`${url}/api/city/${this.state.search}`).then(
-    Axios.get(`${url}/api/users`).then(
+    await Axios.get(`${url}/api/users`).then(
       results => {
-        let allUsers = results.data
-        console.log('ALL USERS====', allUsers)
-        this.setState({
-          numberOfResults: results.data.length,
-          results: allUsers,
+        let allUsers = results.data.user
+        console.log('All results==:',allUsers)
+        allUsers.map((eachUser)=>{
+          console.log('single user:', eachUser.ownerName)
+          console.log('current search:', this.state.search)
+
+          if(eachUser.city === this.state.search){
+            return(
+              <div><h1>
+                {eachUser.ownerName}
+              </h1></div>
+            )
+          }
         })
+
+        console.log('finished push', this.state.results)
+
+
+        // this.setState({
+        //   numberOfResults: results.data.length,
+        //   results: allUsers,
+        // })
       }
     )
   }
 
-  // displayUsers = () => {
-  //   return this.state.results.map((eachUser,i) => {
-  //     return(
-  //     <Link key={i} to={`/user/${eachUser._id}`}>
-  //     <div className="search-result">
-  //     {eachUser.ownerName}
-  //     </div></Link>)
-  //   })
-  // }
+  displayUsers = () => {
+
+    console.log('current search term:', this.state.search)
+    // return this.state.results.map((eachUser,i) => {
+
+    //   // if(eachUser[i].city==='Miami'){
+    //   //   return(
+    //   //     <div>{eachUser.ownerName}</div>
+    //   //   )
+    //   // }
+    //   // return(
+    //   // <Link key={i} to={`/user/${eachUser._id}`}>
+    //   // <div className="search-result">
+    //   // {eachUser.ownerName}
+    //   // </div></Link>)
+    // })
+  }
 
   handleChange = e => {
     this.setState({
@@ -54,7 +79,7 @@ class Search extends Component {
         {/* <button type="text" onClick={this.search}>
           Search
         </button> */}
-        {this.search()}
+        {/* {this.search()} */}
         <button type="submit" onClick={this.search}><i className="fa fa-search"></i></button>
         <span>
           <h2>Number of results: {this.state.numberOfResults}</h2>
