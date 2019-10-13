69b1bfdb6dce54395fccd15bebab9b40a0a584ee
alejandrogomez <alegomez1@me.com>
2019-10-12 23:08:23 -0400

    added image carousel component, but it does not swipe alone

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 import api from '../api'
-
+import ImageCarousel from './ImageCarousel';
 
 export default class Home extends Component {
   render() {
 
     return (
       <div className="Home">
-        <h2>Welcome to Pawnder! {this.props.user.username}</h2>
-        
+        {/* <h2>Welcome to Pawnder! {this.props.user.username}</h2> */}
+        <ImageCarousel/>
       </div>
     )
   }
