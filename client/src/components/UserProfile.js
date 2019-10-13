c02c029db355f546923780626437ef6e528afa9f
alejandrogomez <alegomez1@me.com>
2019-10-12 22:20:46 -0400

    experimenting with colors and minor style changes

--- client/src/components/UserProfile.js
+++ client/src/components/UserProfile.js
@@ -38,59 +38,20 @@ class UserProfile extends Component {
         dogBio: current.dogBio,
         dogAge: current.dogAge,
       })
-
-      // await Axios.get(`${url}/api/dog/${current._id}`).then(result => {
-      //   let dog = result.data
-
-      //   if (dog !== null) {
-      //     console.log('dog result', dog)
-      //     this.setState({
-      //       dogImage: dog.dogImage,
-      //       dogName: dog.dogName,
-      //       dogBio: dog.dogBio,
-      //       dogAge: dog.dogAge,
-      //       dogActivities: dog.dogActivities,
-            
-      //     })
-      //   }
-      // })
     }
   }
-  // checkHasPet = () => {
-  //   if (this.state.dogName !== '') {
-  //     return (
-  //       <div>
-  //         <h2>Dog Profile Page</h2>
-  //         <img src={this.state.dogImage} alt="dogImage" />
-  //         <h4>Name: {this.state.dogName}</h4>
-  //         <p>Bio: {this.state.dogBio}</p>
-  //         <p>Age: {this.state.dogAge}</p>
-  //         <p>Activities: {this.state.dogActivities}</p>
-  //       </div>
-  //     )
-  //   } else {
-  //     return (
-  //       <div>
-  //         <NavLink to="/addPet">
-  //           <button>Add Pet</button>
-  //         </NavLink>
-  //         <br />
-  //       </div>
-  //     )
-  //   }
-  // }
   render() {
     return (
-      <div>
-        {/* {this.checkHasPet()} */}
-        <img src={this.state.dogImage} alt="dogImage" />
+      <div className="profile-div">
+      
+        <img className="profile-dog-image" src={this.state.dogImage} alt="dogImage" />
         <h4>{this.state.dogName}</h4>
-        <p>Bio: {this.state.dogBio}</p>
+        <p>About: {this.state.dogBio}</p>
         <p>Age: {this.state.dogAge}</p>
 
         <img src={this.state.ownerImage} alt="ownerImage" />
         <h4>{this.state.ownerName}</h4>
-        <p>Bio: {this.state.ownerBio}</p>
+        <p>About: {this.state.ownerBio}</p>
         <p>City: {this.state.city}</p>
         <p>Age: {this.state.ownerAge}</p>
 
