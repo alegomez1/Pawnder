15f48ab226c740074465d38ecb123fbf3e836431
alejandrogomez <alegomez1@me.com>
2019-10-12 23:43:22 -0400

    minor tweaks

--- client/src/components/ImageCarousel.js
+++ client/src/components/ImageCarousel.js
@@ -5,12 +5,11 @@ import image1 from './images/image1.jpg'
 import image2 from './images/image2.png'
 import image3 from './images/image3.png'
 
-
 class ImageCarousel extends Component {
     render() {
         return (
-            
-            <Carousel infiniteLoop={true} interval={4000} showThumbs={false} width={'1080px'} autoPlay={true} transitionTime={500}>
+            //********Change autoPlay to false when done */
+            <Carousel infiniteLoop={true} interval={4000} showThumbs={false} width={'1080px'} autoPlay={false} transitionTime={500} stopOnHover={false}>
                 <div class>
                     <img src={image1} />
                     {/* <p className="legend">Legend 1</p> */}
