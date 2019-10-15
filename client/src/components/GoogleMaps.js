8e4f0458b67e6c541be82b30b5bcbf89f3dcdfca
alejandrogomez <alegomez1@me.com>
2019-10-15 14:33:29 -0400

    Added same styling to unique user

--- client/src/components/GoogleMaps.js
+++ client/src/components/GoogleMaps.js
@@ -42,8 +42,6 @@ export class GoogleMaps extends Component {
       >
         <Marker onClick={this.onMarkerClick} name={'Miami International Airport'} />
         <Marker position ={{lat: 25.769241,lng: -80.199099 } }onClick={this.onMarkerClick} name={'Test'} />
-
-
         <InfoWindow
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
