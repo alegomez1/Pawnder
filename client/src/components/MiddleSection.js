b9bd6110e261570aec62e68e21ee5bdf410eb308
alejandrogomez <alegomez1@me.com>
2019-10-16 14:36:04 -0400

    middle section looks done for now

--- client/src/components/MiddleSection.js
+++ client/src/components/MiddleSection.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
 import form from './images/form.png'
 import search from './images/magnifyingglass.png'
 import dogs from './images/doggroup.png'
@@ -7,12 +9,14 @@ class MiddleSection extends Component {
   render() {
     return (
       <div className="middle-section container-2">
-        <h1 id='how-it-works' className="align-center">How it works</h1>
+        <h1 id="how-it-works" className="align-center">
+          How it works
+        </h1>
         <div className="row middle-row">
           <div className="col">
-            <h2 className="align-center">Sign Up</h2>
+            <h2 className="align-center middle-headers">Sign Up</h2>
             <div className="image-col">
-              <p className="align-center">
+              <p className="align-center middle-paragraphs">
                 Sign up in less than 5 minutes using
                 <br /> our interactive form
               </p>
@@ -20,9 +24,9 @@ class MiddleSection extends Component {
             </div>
           </div>
           <div className="col">
-            <h2 className="align-center">Search for your city</h2>
+            <h2 className="align-center middle-headers">Search for your city</h2>
             <div className="image-col">
-              <p className="align-center">
+              <p className="align-center middle-paragraphs">
                 Look through the results to find the one that best matches your
                 pet's needs
               </p>
@@ -30,9 +34,9 @@ class MiddleSection extends Component {
             </div>
           </div>
           <div className="col">
-            <h2 className="align-center">Find a playdate!</h2>
+            <h2 className="align-center middle-headers">Find a playdate!</h2>
             <div className="image-col">
-              <p className="align-center">
+              <p className="align-center middle-paragraphs">
                 Once you find a playpal, send them an email
                 <br /> and schedule a playdate!
               </p>
@@ -40,7 +44,7 @@ class MiddleSection extends Component {
             </div>
           </div>
         </div>
-      {/* Images section */}
+        {/* Images section */}
         <div className="row">
           <div className="col">
             <div className="image-cont">
@@ -57,6 +61,16 @@ class MiddleSection extends Component {
           <div className="col">
             <div className="image-cont">
               <img className="middle-image-3" src={dogs} alt="formIcon" />
+            </div>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col">
+            <div className="cover-image-button-div button-row">
+              <Link className="testlink testbutton" to="/signup">
+                <button className="cover-image-button-2">Get started</button>
+              </Link>
             </div>
           </div>
         </div>
